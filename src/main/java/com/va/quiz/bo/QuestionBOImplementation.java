f5c976f5629a6ef3f2e62d293f4ae4a2097c663a
Aonozan <aonozan@gmail.com>
2017-02-25 18:01:52 +0100

    Question tests setup.

--- src/main/java/com/va/quiz/bo/QuestionBOImplementation.java
+++ src/main/java/com/va/quiz/bo/QuestionBOImplementation.java
@@ -1,7 +1,15 @@
 
 package com.va.quiz.bo;
+
+import com.va.quiz.dao.QuestionDAO;
+
 /**
  *  @author AonoZan Dejan Petrovic 2017 ©
  */
 public class QuestionBOImplementation implements QuestionBO {
+	private QuestionDAO dao;
+
+	public void setDao(QuestionDAO dao) {
+		this.dao = dao;
+	}
 }
--- src/test/java/com/va/quiz/QuestionBOImplementationTest.java
+++ src/test/java/com/va/quiz/QuestionBOImplementationTest.java
@@ -1,9 +1,28 @@
 
 package com.va.quiz;
 
+import org.junit.Before;
+import org.mockito.Mockito;
+
+import com.va.quiz.bo.QuestionBOImplementation;
+import com.va.quiz.dao.QuestionDAO;
+import com.va.quiz.dto.Question;
+
 /**
  *  @author AonoZan Dejan Petrovic 2017 ©
  */
 public class QuestionBOImplementationTest {
+	QuestionDAO daoMock = Mockito.mock(QuestionDAO.class);
+	QuestionBOImplementation bo = new QuestionBOImplementation();
+
+	Question question;
+
+	final int EDITOR = 1;
+
+	@Before
+	public void setUp() {
+		bo.setDao(daoMock);
+		question = new Question(EDITOR);
+	}
 }
 
