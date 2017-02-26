94f36c726170a3e89919d1bad624da5e0033b5e8
Aonozan <aonozan@gmail.com>
2017-02-26 07:27:18 +0100

    Score setup.

--- src/main/java/com/va/quiz/bo/ScoreBOImplementation.java
+++ src/main/java/com/va/quiz/bo/ScoreBOImplementation.java
@@ -1,7 +1,14 @@
-
 package com.va.quiz.bo;
+
+import com.va.quiz.dao.ScoreDAO;
+
 /**
  *  @author AonoZan Dejan Petrovic 2017 ©
  */
 public class ScoreBOImplementation implements ScoreBO {
+	ScoreDAO dao;
+
+	public void setDao(ScoreDAO dao) {
+		this.dao = dao;
+	}
 }
--- src/test/java/com/va/quiz/ScoreBOImplementationTest.java
+++ src/test/java/com/va/quiz/ScoreBOImplementationTest.java
@@ -1,8 +1,26 @@
-
 package com.va.quiz;
+
+import org.junit.Before;
+import org.mockito.Mockito;
+
+import com.va.quiz.bo.ScoreBOImplementation;
+import com.va.quiz.dao.ScoreDAO;
+import com.va.quiz.dto.Score;
+
 /**
  *  @author AonoZan Dejan Petrovic 2017 ©
  */
 public class ScoreBOImplementationTest {
-}
+	ScoreDAO daoMock = Mockito.mock(ScoreDAO.class);
+	ScoreBOImplementation bo = new ScoreBOImplementation();
 
+	Score score;
+
+	final int USER_ID = 1, WRONG_ID = -1;
+
+	@Before
+	public void setUp() {
+		bo.setDao(daoMock);
+		score = new Score(USER_ID);
+	}
+}
